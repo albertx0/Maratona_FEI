4e684d5d8e8fd90b7ab28285e610679e3fbc72aa
albertx0 <126724209+albertx0@users.noreply.github.com>
2024-03-10 00:57:37 -0300

    Create subarraySum.cpp

---
+++ subarraySum.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    	ios::sync_with_stdio(0);
+    	cin.tie(0);
+
+        //Algoritmo para calcular a maior soma de um subarray
+        //Complexidade O(n)
+
+        int melhorSoma = 0;
+
+        int lista[] = {-1 , 2 , 4 , -3 , 5 , 2 , -5 , 2};
+        int n = 8;
+        int soma = 0;
+        for(int i = 0 ; i < n ; i++){
+            soma = max(lista[i] , soma + lista[i]);
+            melhorSoma = max(melhorSoma , soma);
+        }
+
+    	cout << melhorSoma << endl;
+}
